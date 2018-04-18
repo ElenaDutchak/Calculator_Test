202a8e9ce0e96cb342f63ef3155d9581767a1d1e
Elena Dutchak <elenadutchak6@gmail.com>
2018-04-18 17:08:30 +0300

    Commit

--- OurCoolTest.java
+++ OurCoolTest.java
@@ -1,33 +1,26 @@
 package calculator;
 
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.AssertJUnit.assertTrue;
 
 public class OurCoolTest {
 
-    @BeforeClass
-    public void before() {
-        System.out.println("Before class method");
+    @DataProvider(name = "calculatorDataProvider")
+    public Object[][] simpleDataProvider() {
+        return new Object[][]{
+                {2, 2, 4},
+                {2, 0, 0},
+                {-1, 2, -2},
+                {0, 2, 0}
+        };
     }
-    @Test
-    public void checkParameters() {
-        System.out.println("Test method");
-        Calculator calculator = new Calculator(2, 2);
+    @Test(dataProvider = "calculatorDataProvider")
+    public void checkParameters(int firstParam, int secondParam, int expectedResult) {
+        Calculator calculator = new Calculator(firstParam, secondParam);
         int result = calculator.getResult();
-        assertTrue("Результат(" + result + ") не равен 4", result == 4);
+        assertTrue("Результат(" + result + ") не равен " + expectedResult, result == expectedResult);
     }
-    @Test
-    public void checkMoreParameters() {
-        System.out.println("Second test method");
-        Calculator calculator = new Calculator(3, 3);
-        int result = calculator.getResult();
-        assertTrue("Результат(" + result + ") не равен 9", result == 9);
-    }
-    @AfterClass
-    public void after() {
-        System.out.println("After class method");
-    }
+
 }
